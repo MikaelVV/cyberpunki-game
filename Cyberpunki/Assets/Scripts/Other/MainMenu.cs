58f3aad62a906b8936b8aa2e7290187db2d48618
MikaelVV <RIH4MN2@gmail.com>
2020-07-19 19:33:25 +0300

    Update #1

---
+++ Cyberpunki/Assets/Scripts/Other/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene("01");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ContinueGame()
+    {
+        //SceneManager.LoadScene("Taso04");
+    }
+
+}
