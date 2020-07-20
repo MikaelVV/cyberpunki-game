2b8be4442e6ec8f8ba4dbe6dbd3d49c58dd20caf
MikaelVV <RIH4MN2@gmail.com>
2020-07-21 01:18:29 +0300

    Update #2

--- Cyberpunki/Assets/Scripts/Player/PlayerMovement.cs
+++ Cyberpunki/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class Scripts : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody2D pelaaja;
+
+    public float moveSpeed = 5f;
+    public float speed;
+
+    private bool FacingRight = true;
+
 
     void Start()
     {
-        
+        pelaaja = GetComponent<Rigidbody2D>();
     }
-
 
     void Update()
     {
-        
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 input = new Vector3(x, y, 0);
+
+        transform.position += input * moveSpeed;
+
+
+        if (x < 0 && !FacingRight)
+        {
+            Flip();
+        }
+
+        else if (x > 0 && FacingRight)
+        {
+            Flip();
+        }
     }
+
+    void Flip()
+    {
+        FacingRight = !FacingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
 }
