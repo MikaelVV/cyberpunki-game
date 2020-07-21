6516f89e3724133d066e7f07140dbf19fff04870
MikaelVV <RIH4MN2@gmail.com>
2020-07-21 18:03:31 +0300

    Update #3

---
+++ Cyberpunki/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+
+    public void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+
+    public void LateUpdate()
+    {
+
+
+        transform.position = player.transform.position + offset;
+
+    }
+
+
+}
+
